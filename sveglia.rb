fdc599d14d5c9c90c760fdd76e3ab3a1956be964
Giovanni Cappellotto <potomak84@gmail.com>
2012-02-11 12:36:31 +0100

    Add templates handling and routing
    See more about there features at
    
    * https://github.com/postrank-labs/goliath/blob/master/examples/rack_routes.rb
    * https://github.com/postrank-labs/goliath/blob/master/examples/template.rb

--- sveglia.rb
+++ sveglia.rb
@@ -1,13 +1,29 @@
 require 'goliath'
+require 'goliath/rack/templates'
+require 'goliath/plugins/latency'
 require 'em-synchrony/em-http'
 require 'em-http/middleware/json_response'
 require 'yajl'
+require 'tilt'
+require 'haml'
+
+class Template < Goliath::API
+  include Goliath::Rack::Templates      # render templated files from ./views
+  plugin Goliath::Plugin::Latency       # ask eventmachine reactor to track its latency
+
+  def recent_latency
+    Goliath::Plugin::Latency.recent_latency
+  end
+
+  def response(env)
+    [200, {}, haml(:index)]
+  end
+end
 
 # automatically parse the JSON HTTP response
 EM::HttpRequest.use EventMachine::Middleware::JSONResponse
 
 class Hello < Goliath::API
-
   # parse query params and auto format JSON response
   use Goliath::Rack::Params
   use Goliath::Rack::Formatters::JSON
@@ -27,5 +43,24 @@ class Hello < Goliath::API
     end
 
     [200, {'Content-Type' => 'application/json'}, resp]
+  end
+end
+
+class RackRoutes < Goliath::API
+  # map Goliath API to a specific path
+  get "/", Template
+  get "/hello", Hello
+  
+  use(Rack::Static,                     # render static files from ./public
+      :root => Goliath::Application.app_path("public"),
+      :urls => ["/favicon.ico", '/stylesheets', '/javascripts', '/images'])
+
+  not_found do
+    run Proc.new { |env| [404, {"Content-Type" => "text/html"}, ["Try /version /hello_world, /bonjour, or /hola"]] }
+  end
+
+  # You must use either maps or response, but never both!
+  def response(env)
+    raise "#response is ignored when using maps, so this exception won't raise. See spec/integration/rack_routes_spec."
   end
 end
