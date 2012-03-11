fa3bc997c76de43dd86f4030c6768a23ee107581
Giovanni Cappellotto <potomak84@gmail.com>
2012-03-11 17:26:03 +0100

    Add Timer model stub

---
+++ models/timer.rb
@@ -0,0 +1,22 @@
+class Timer
+  attr_accessor :id, :name
+
+  def initialize(hash={})
+    @id = hash[:id]
+    @name = hash[:name]
+  end
+
+  def self.all
+    (1..10).map do |id|
+      Timer.new(id: id, name: "Timer ##{id}")
+    end
+  end
+
+  def to_hash
+    {id: @id, name: @name}
+  end
+
+  def to_json
+    JSON.generate(to_hash)
+  end
+end
